59fca10d3fc8abfefa970a225435f117cfcac786
chrispydev <christianowusu44@gmail.com>
2025-12-12 22:10:17 +0000

    feat(mobile sidebar): add mobile side bar

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+// Get DoM elements
+// ge the element when the windows is loaded
+window.onload = function () {
+  const closeBtn = document.getElementById('closebtn');
+
+  console.log(closeBtn)
+
+}
