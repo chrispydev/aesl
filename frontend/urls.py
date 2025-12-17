9af6e42faf28eade7e7c1b8fdec9a79d40d44c45
chrispydev <christianowusu44@gmail.com>
2025-12-17 22:17:33 +0000

    feat(projects): add projects text

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from frontend.views import HomeView
+from frontend.views import HomeView, ProjectView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("projects/", ProjectView.as_view(), name="projects"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -4,7 +4,11 @@ from django.shortcuts import render
 
 class HomeView(View):
     def get(self, request):
-        context = {
-            'title': "HomePage"
-        }
+        context = {"title": "HomePage"}
         return render(request, "frontend/home.html", context)
+
+
+class ProjectView(View):
+    def get(self, request):
+        context = {"title": "Projects"}
+        return render(request, "frontend/projects.html", context)
