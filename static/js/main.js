5de32bd50c5ce53904e3b1c36fdb1226d884d7fa
chrispydev <christianowusu44@gmail.com>
2026-01-01 20:18:09 +0000

    fix(sidenav): mixed class name

--- static/js/main.js
+++ static/js/main.js
@@ -1,32 +1,35 @@
 const toggleDiv = document.querySelector(".mobile__menuoverlay");
 const closeBtn = document.querySelector(".closebtn");
-const showSidebar = document.querySelector(".mobile__menubtn")
-
+const showSidebar = document.querySelector(".mobile__menubtn");
 
 closeBtn.addEventListener("click", function () {
-  toggleDiv.classList.remove('toggle__sidebar')
-
+    toggleDiv.classList.remove("toggle__sidebar");
 });
 
 showSidebar.addEventListener("click", function () {
-  toggleDiv.classList.add('toggle__sidebar')
-})
-
-const galleryItems = document.querySelectorAll('.projects__image');
-galleryItems.forEach(item => {
-  const rowHeight = parseInt(
-    window.getComputedStyle(document.querySelector('.projects__masonry-gallery'))
-      .getPropertyValue('grid-auto-rows')
-  );
-  const rowGap = parseInt(
-    window.getComputedStyle(document.querySelector('.projects__masonry-gallery'))
-      .getPropertyValue('gap')
-  );
-  const imgHeight = item.querySelector('img').getBoundingClientRect().height;
-  const rowSpan = Math.ceil((imgHeight + rowGap) / (rowHeight + rowGap));
-  item.style.gridRowEnd = `span ${rowSpan}`;
+    toggleDiv.classList.add("toggle__sidebar");
 });
 
+const galleryItems = document.querySelectorAll(".projects__image");
+galleryItems.forEach((item) => {
+    const rowHeight = parseInt(
+        window
+            .getComputedStyle(
+                document.querySelector(".projects__masonry-gallery"),
+            )
+            .getPropertyValue("grid-auto-rows"),
+    );
+    const rowGap = parseInt(
+        window
+            .getComputedStyle(
+                document.querySelector(".projects__masonry-gallery"),
+            )
+            .getPropertyValue("gap"),
+    );
+    const imgHeight = item.querySelector("img").getBoundingClientRect().height;
+    const rowSpan = Math.ceil((imgHeight + rowGap) / (rowHeight + rowGap));
+    item.style.gridRowEnd = `span ${rowSpan}`;
+});
 
 const sideNav = document.querySelector(".sector__aside");
 const footer = document.querySelector(".site__footer");
@@ -35,29 +38,28 @@ const triggerPoint = 600;
 let footerVisible = false;
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY >= triggerPoint && !footerVisible) {
-    sideNav.classList.add("active");
-  } else {
-    sideNav.classList.remove("active");
-  }
+    if (window.scrollY >= triggerPoint && !footerVisible) {
+        sideNav.classList.add("active");
+    } else {
+        sideNav.classList.remove("active");
+    }
 });
 
 // Observe footer
 const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach(entry => {
-      footerVisible = entry.isIntersecting;
+    (entries) => {
+        entries.forEach((entry) => {
+            footerVisible = entry.isIntersecting;
 
-      if (footerVisible) {
-        sideNav.classList.remove("active");
-      }
-    });
-  },
-  {
-    root: null,
-    threshold: 0.1
-  }
+            if (footerVisible) {
+                sideNav.classList.remove("active");
+            }
+        });
+    },
+    {
+        root: null,
+        threshold: 0.1,
+    },
 );
 
 observer.observe(footer);
-
