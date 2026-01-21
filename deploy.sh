a06fd5fefb9d635c7570b4eb492d145c7d193d6d
chrispydev <christianowusu44@gmail.com>
2026-01-21 23:46:14 +0000

    chore(gitignore): add git ignore

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-cd /home/aesl
-source venv/bin/activate
-git pull origin main
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput
-sudo systemctl restart gunicorn
